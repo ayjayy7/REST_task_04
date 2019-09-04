8793ae1e9fe90d682000c4fa1f1c1337947ab0c7
ayjayy7 <eng.amonijoni@gmail.com>
2019-09-04 11:53:45 +0300

    rest task 4

--- flights/serializers.py
+++ flights/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 
 from .models import Flight, Booking
+from django.contrib.auth.models import User
+from rest_framework import serializers
 
 
 class FlightSerializer(serializers.ModelSerializer):
@@ -26,3 +28,20 @@ class UpdateBookingSerializer(serializers.ModelSerializer):
 		model = Booking
 		fields = ['date', 'passengers']
 
+
+
+# 4- Create a view for registering users:
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'first_name', 'last_name']
+
+    def create(self, validated_data):
+        username = validated_data['username']
+        password = validated_data['password']
+        first_name = validated_data['first_name']
+        last_name = validated_data['last_name']
+        new_user = User(username=username,first_name=first_name,last_name=last_name)
+        new_user.set_password(password)
+        new_user.save()
+        return validated_data
